0a3409b29d98a4e2fd8c3826797ac402e05d0335
Elizabeth Zevin <ehzevin@gmail.com>
2019-03-14 17:48:31 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -10,8 +10,8 @@ class Project
   end
 
   def add_backer(backer)
-      @backers << backer
-      backer.backed_projects << self
+      # @backers << backer
+      # backer.backed_projects << self
   end
 
 
